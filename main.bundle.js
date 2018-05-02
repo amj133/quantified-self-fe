f19b8a10a3b5c32038261896296c53c717600532
amj133 <amj@vt.edu>
2018-05-02 14:03:01 -0600

    run npm run build

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://calm-fjord-39732.herokuapp.com/api/v1/meals";
+	    return "https://calm-fjord-39732.herokuapp.com";
 	  }
 	};
 
