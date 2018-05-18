4c84ad56f447d837b4d2a86fb67898be0d0c1c12
amj133 <amj@vt.edu>
2018-05-17 20:14:37 -0600

    npm run

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://desolate-peak-32053.herokuapp.com";
+	    return "https://stormy-fortress-30140.herokuapp.com";
 	  }
 	};
 
